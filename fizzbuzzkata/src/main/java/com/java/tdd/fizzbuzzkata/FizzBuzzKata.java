143fbd5c5260473d45c737c560cdf9cf335643d9
keerthanaces <119451000+keerthanaces@users.noreply.github.com>
2023-01-23 20:22:57 +0530

    intial commit for tdd test-digitzclub

---
+++ fizzbuzzkata/src/main/java/com/java/tdd/fizzbuzzkata/FizzBuzzKata.java
@@ -0,0 +1,42 @@
+package com.java.tdd.fizzbuzzkata;
+
+/**
+ *FizzBuzzKata class performs following logic
+ *Write a program that prints one line for each number from 1 to 100
+ *Usually just print the number itself.
+ *For multiples of three print Fizz instead of the number
+ *For the multiples of five print Buzz instead of the number
+ *For numbers which are multiples of both three and five print FizzBuzz instead of the number
+ */
+
+public class FizzBuzzKata 
+{
+    public static void main( String[] args )
+    {
+       for(int i=1;i<=100;i++ ) {
+    	   System.out.println(convertToFizzBuzz(i));
+       }
+    }
+
+	/**
+	 * convertToFizzBuzz method handle logic for FizzBuzzKata program
+	 * 
+	 * @param convertFizzBuzzValue
+	 * @return convertedFizzBuzzValue
+	 */
+	public static String convertToFizzBuzz(int convertFizzBuzzValue) {
+		if(convertFizzBuzzValue%3==0 && convertFizzBuzzValue %5==0) {
+			return "FizzBuzz";
+		}
+		else if(convertFizzBuzzValue%3==0) {
+			return "Fizz";
+		}
+		else if(convertFizzBuzzValue%5==0){
+			return "Buzz";
+		}
+		else {
+			return String.valueOf(convertFizzBuzzValue);
+		}
+	}
+	
+}
---
+++ fizzbuzzkata/src/test/java/com/java/tdd/fizzbuzzkata/FizzBuzzKataTest.java
@@ -0,0 +1,31 @@
+package com.java.tdd.fizzbuzzkata;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for FizzBuzzKata class to perform following logic
+ * 
+ * Write a program that prints one line for each number from 1 to 100
+ * Usually just print the number itself.
+ * For multiples of three print Fizz instead of the number
+ * For the multiples of five print Buzz instead of the number
+ * For numbers which are multiples of both three and five print FizzBuzz instead of the number
+ */
+
+public class FizzBuzzKataTest extends TestCase
+{
+    /**
+     *  Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertEquals("1", FizzBuzzKata.convertToFizzBuzz(1));
+        assertEquals("2", FizzBuzzKata.convertToFizzBuzz(2));
+        assertEquals("Fizz", FizzBuzzKata.convertToFizzBuzz(3));
+        assertEquals("Buzz", FizzBuzzKata.convertToFizzBuzz(5));
+        assertEquals("FizzBuzz", FizzBuzzKata.convertToFizzBuzz(15));
+        assertEquals("19", FizzBuzzKata.convertToFizzBuzz(19));
+        assertEquals("Fizz", FizzBuzzKata.convertToFizzBuzz(21));
+        
+    }
+}
