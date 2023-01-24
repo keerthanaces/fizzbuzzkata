77b0f1d99e70db00bc5692f02dd651548a4cbd80
keerthanaces <keerthanacse4@gmail.com>
2023-01-24 22:35:58 +0530

    format

--- fizzbuzzkata/src/main/java/com/java/tdd/fizzbuzzkata/FizzBuzzKata.java
+++ fizzbuzzkata/src/main/java/com/java/tdd/fizzbuzzkata/FizzBuzzKata.java
@@ -1,22 +1,19 @@
 package com.java.tdd.fizzbuzzkata;
 
 /**
- *FizzBuzzKata class performs following logic
- *Write a program that prints one line for each number from 1 to 100
- *Usually just print the number itself.
- *For multiples of three print Fizz instead of the number
- *For the multiples of five print Buzz instead of the number
- *For numbers which are multiples of both three and five print FizzBuzz instead of the number
+ * FizzBuzzKata class performs following logic Write a program that prints one
+ * line for each number from 1 to 100 Usually just print the number itself. For
+ * multiples of three print Fizz instead of the number For the multiples of five
+ * print Buzz instead of the number For numbers which are multiples of both
+ * three and five print FizzBuzz instead of the number
  */
 
-public class FizzBuzzKata 
-{
-    public static void main( String[] args )
-    {
-       for(int i=1;i<=100;i++ ) {
-    	   System.out.println(convertToFizzBuzz(i));
-       }
-    }
+public class FizzBuzzKata {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 100; i++) {
+			System.out.println(convertToFizzBuzz(i));
+		}
+	}
 
 	/**
 	 * convertToFizzBuzz method handle logic for FizzBuzzKata program.
@@ -25,18 +22,15 @@ public class FizzBuzzKata
 	 * @return convertedFizzBuzzValue
 	 */
 	public static String convertToFizzBuzz(int convertFizzBuzzValue) {
-		if(convertFizzBuzzValue%3==0 && convertFizzBuzzValue %5==0) {
+		if (convertFizzBuzzValue % 3 == 0 && convertFizzBuzzValue % 5 == 0) {
 			return "FizzBuzz";
-		}
-		else if(convertFizzBuzzValue%3==0) {
+		} else if (convertFizzBuzzValue % 3 == 0) {
 			return "Fizz";
-		}
-		else if(convertFizzBuzzValue%5==0){
+		} else if (convertFizzBuzzValue % 5 == 0) {
 			return "Buzz";
-		}
-		else {
+		} else {
 			return String.valueOf(convertFizzBuzzValue);
 		}
 	}
-	
+
 }
